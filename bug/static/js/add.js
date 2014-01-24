39ef294f7b38cc7416eca3fa65dcb12e0179e946
david <david.lucky.star@gmail.com>
2014-01-24 14:55:40 +0400

    upd

--- bug/static/js/add.js
+++ bug/static/js/add.js
@@ -14,49 +14,59 @@ $(function() {
 	// datepicker
 	$('#birthDate').datepicker({
 		format: "mm/dd/yyyy",
-		startDate: '-20d'
+		startDate: '01/01/1950',
+		startView: 'year'
 	})
 	// validations
 	jQuery.validator.setDefaults({
 		debug: true,
 		success: "valid"
 	})
-	// $('form').validate({
-	// 	rules: {
-	// 		name: {
-	// 			required: true
-	// 		},
-	// 		email: {
-	// 			required: true,
-	// 			email: true
-	// 		},
-	// 		birthDate: {
-	// 			required: true,
-	// 			date: true
-	// 		},
-	// 		position: {
-	// 			required: true
-	// 		},
-	// 		permission: {
-	// 			required: true
-	// 		}
-	// 	},
-	// 	errorElement: 'span',
-	//     errorClass: 'help-block',
-	//     errorPlacement: function(error, element) {
-	//         if(element.parent('.input-group').length) {
-	//             error.insertAfter(element.parent());
-	//         } else {
-	//             error.insertAfter(element);
-	//         }
-	//     },
-	// 	highlight: function(element) {
- //      		$(element).closest('.form-group').addClass('has-error');
-	//     },
-	//     unhighlight: function(element) {
-	//         $(element).closest('.form-group').removeClass('has-error');
-	//     }
-	// })
+	$.validator.addMethod(
+        "regex",
+        function(value, element, regexp) {
+            var re = new RegExp(regexp);
+            return this.optional(element) || re.test(value);
+        },
+        "Please check your input."
+	)
+	$('form').validate({
+		rules: {
+			name: {
+				required: true
+			},
+			email: {
+				required: true,
+				email: true,
+				regex: '[a-zA-Z.@]{1,40}'
+			},
+			birthDate: {
+				required: true,
+				date: true
+			},
+			position: {
+				required: true
+			},
+			permission: {
+				required: true
+			}
+		},
+		errorElement: 'span',
+	    errorClass: 'help-block',
+	    errorPlacement: function(error, element) {
+	        if(element.parent('.input-group').length) {
+	            error.insertAfter(element.parent());
+	        } else {
+	            error.insertAfter(element);
+	        }
+	    },
+		highlight: function(element) {
+      		$(element).closest('.form-group').addClass('has-error');
+	    },
+	    unhighlight: function(element) {
+	        $(element).closest('.form-group').removeClass('has-error');
+	    }
+	})
 	
 })
 
